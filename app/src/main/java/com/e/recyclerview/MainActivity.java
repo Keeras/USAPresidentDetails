78bcb3954c3868a4b6b5865f894d9428f6818c0f
Keeras <keerasmhz@gmail.com>
2019-11-11 15:03:56 +0545

    New Project

---
+++ app/src/main/java/com/e/recyclerview/MainActivity.java
@@ -0,0 +1,34 @@
+package com.e.recyclerview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import adapter.PresidentAdapter;
+import model.President;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+
+        //create a list of contacts
+        List<President> presidentList = new ArrayList<>();
+        presidentList.add(new President("Donald Trump","God   father",R.drawable.ic_launcher_background));
+        presidentList.add(new President("George H. W. Bush","dfkdls",R.drawable.ic_launcher_background));
+
+
+///       Pass List to adapter class
+        PresidentAdapter presidentAdapter = new PresidentAdapter(this, presidentList);
+        recyclerView.setAdapter(presidentAdapter);
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
+    }
+}
